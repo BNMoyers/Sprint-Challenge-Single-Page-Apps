b887391d16fb874439dc64b71a4c5e0deaa2f73d
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2019-07-26 10:37:25 -0500

    save point; setting up API link, state, and useEffect

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,22 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 export default function CharacterList() {
-  // TODO: Add useState to track data from useEffect
+ const [characters, setCharacters] = useState([]);
 
   useEffect(() => {
     // TODO: Add AJAX/API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, [])
+    const getCharacters = () => {
+
+    }
+  }, [characters])
 
   return <section className='character-list grid-view'>
 
-      <h2>TODO: `array.map()` over your state here!</h2>
+      {characters.map(character => (
+
+      ))}
     </section>
 
 }
