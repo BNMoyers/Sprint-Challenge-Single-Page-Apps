f4de9e57d5fdecddb93471486a7a79ea5eef15b1
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2019-07-26 11:51:56 -0500

    finished basic character cards

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,22 +1,38 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import CharacterCard from './CharacterCard';
+import { Grid, Image } from 'semantic-ui-react'
 
 export default function CharacterList() {
  const [characters, setCharacters] = useState([]);
 
   useEffect(() => {
-    // TODO: Add AJAX/API Request here - must run in `useEffect`
-    const getCharacters = () => {
+    axios
+    .get("https://rickandmortyapi.com/api/character/")
+    .then(response => {
+      setCharacters(response.data.results);
+    })
+    .catch(error => {
+      console.error('error', error)
+    })
+     }, [])
 
-    }
-  }, [characters])
-
-  return <section className='character-list grid-view'>
-
-      {characters.map(character => (
+  return ( 
+  <section className='character-list grid-view'>
+    <Grid centered columns={3} >
+      {characters.map(results => (
+        <Grid.Column width={5}>
+          <CharacterCard  
+              image={results.image}
+              name = {results.name}
+              species={results.species}
+              status = {results.status}
+              location = {results.location}/>
+              </Grid.Column>
 
       ))}
+      </Grid>
     </section>
-
+  )
 }
