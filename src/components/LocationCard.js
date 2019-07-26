3100c4624a1261bc7fac4bf1fb7224f45fc30545
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2019-07-26 12:07:04 -0500

    location files finished

--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -1,7 +1,19 @@
 import React from 'react'
+import { Cards } from 'semantic-ui-react'
 
-export default function LocationCard ({ name, type, dimension, residents }) {
-  // image={image}
-  return (<span>todo: location</span>
+export default function LocationCard (props) {
+  console.log(props)
+  return(
+    <div class="ui cards">
+    <div class="card">
+      <div class="content">
+        <div class="header">{props.name}</div>
+        <div class="meta">{props.dimension}</div>
+        <div class="description">
+          Residents: {props.residents}
+        </div>
+      </div>
+    </div>
+    </div>
   )
 }
--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,5 +1,39 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import LocationCard from './LocationCard';
+import { Grid, Image } from 'semantic-ui-react'
 
-export default function LocationsList() {
+export default function LocationList() {
+ const [locations, setLocations] = useState([]);
 
+  useEffect(() => {
+    axios
+    .get("https://rickandmortyapi.com/api/location/")
+    .then(response => {
+      setLocations(response.data.results);
+    })
+    .catch(error => {
+      console.error('error', error)
+    })
+     }, [])
+
+  return ( 
+  <section className='location-list grid-view'>
+    <Grid centered columns={3} >
+      {locations.map(results => (
+        <Grid.Column width={5}>
+          <LocationCard  
+              name = {results.name}
+              dimension={results.dimension}
+              residents={results.residents.length}/>
+              </Grid.Column>
+
+      ))}
+      </Grid>
+    </section>
+  )
 }
+
+
+
